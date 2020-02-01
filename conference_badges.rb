d39ee4c01ace1910c17abbce0b11d96985a2466b
Arif Cagirici <acagirici23@gmail.com>
2020-02-01 01:26:04 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,2 +1,4 @@
 def batch_badge_creator(name)
-speakers = [Edsger, Ada, Charles, Alan, Grace, Linus, and Matz]
+speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus","Matz"]
+rooms = [1,2,3,4,5,6,7]
+end
