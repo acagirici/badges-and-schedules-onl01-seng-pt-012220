5477e49782bacc1fca7936b5353d7e53216014b9
Arif Cagirici <acagirici23@gmail.com>
2020-02-01 01:31:09 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,4 +1,11 @@
 def batch_badge_creator(name)
 speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus","Matz"]
-rooms = [1,2,3,4,5,6,7]
 end
+
+def assign_rooms(room)
+  rooms = [1,2,3,4,5,6,7]
+end
+
+def printer
+  batch_badge_creator.each do |speaker|
+    puts 
